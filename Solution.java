97d77e37552a23fcd6c3e54edd3e45127d46b062
sevoronko <100187336+sevoronko@users.noreply.github.com>
2022-04-12 15:13:38 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        
+        boolean anagrams = true;
+        int lenghtA = a.length();
+        int lenghtB = b.length();
+
+        if (lenghtA == lenghtB) {
+
+            for (int i = lenghtA - 1; i >= 0; i--) {
+                char someChar = a.charAt(i);
+
+                int countA = 0;
+                int countB = 0;
+
+                for (int j = lenghtA - 1; j >= 0; j--) {
+                    char someCharA = a.charAt(j);
+                    if (someChar == someCharA) {
+                        countA++;
+                    }
+                    char someCharB = b.charAt(j);
+                    if (someChar == someCharB) {
+                        countB++;
+                    }
+                }
+                
+                if (countA == countB) {
+                    continue;
+                } else {
+                    anagrams = false;
+                    break;
+                }
+            }
+        } else anagrams = false;
+
+        return anagrams;
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
