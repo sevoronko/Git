48bc413790b754f29d55e8d50cca7d4f98ce02a4
sevoronko <100187336+sevoronko@users.noreply.github.com>
2022-04-13 10:47:54 +0300

    Update JavaStringReverse.java

--- hackerrank.com/JavaStringReverse.java
+++ hackerrank.com/JavaStringReverse.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String B = "";
+        int length = A.length();
+
+        for (int i = length - 1; i >= 0; i--) {
+            char a = A.charAt(i);
+            B = B + a;
+        }
+
+        if (B.compareTo(A) == 0) {
+            System.out.println("Yes");
+        } else System.out.println("No");
+        
+    }
+}
