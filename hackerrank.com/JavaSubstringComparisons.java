a3c333675827006362fddf50bfd236c0cdba4b6e
sevoronko <100187336+sevoronko@users.noreply.github.com>
2022-04-13 10:50:43 +0300

    Update JavaSubstringComparisons.java

--- hackerrank.com/JavaSubstringComparisons.java
+++ hackerrank.com/JavaSubstringComparisons.java
@@ -1,6 +1,26 @@
+/*We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings*/
+
 import java.util.Scanner;
 
-public class Solution {
+public class JavaSubstringComparisons {
 
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
