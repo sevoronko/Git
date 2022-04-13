1216a06dbfd90648fad659560f2b98c3f69860c7
sevoronko <100187336+sevoronko@users.noreply.github.com>
2022-04-13 10:48:36 +0300

    Update JavaStringReverse.java

--- hackerrank.com/JavaStringReverse.java
+++ hackerrank.com/JavaStringReverse.java
@@ -1,3 +1,7 @@
+/*A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+Given a string , print Yes if it is a palindrome, print No otherwise.*/
+
 import java.io.*;
 import java.util.*;
 
